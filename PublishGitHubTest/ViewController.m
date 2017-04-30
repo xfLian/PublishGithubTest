69ca6d20fa1623c9f794aac4fafd27c734775ffd
xfLian <82634907@qq.com>
2017-05-01 04:32:58 +0800

    第一次上传
    上传测试

---
+++ PublishGitHubTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PublishGitHubTest
+//
+//  Created by xf_Lian on 2017/5/1.
+//  Copyright © 2017年 xf_Lian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ PublishGitHubTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PublishGitHubTest
+//
+//  Created by xf_Lian on 2017/5/1.
+//  Copyright © 2017年 xf_Lian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
